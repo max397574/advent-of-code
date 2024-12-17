8ebc29b8421e9587f2fe59af1e924ab5232527ab
Max <max.heinzer397@gmx.ch>
2024-12-17 10:40:05 +0100

    silence warnings

--- src/solutions/year2024/day15.rs
+++ src/solutions/year2024/day15.rs
@@ -6,10 +6,10 @@ pub fn part2(_input: &str) -> impl std::fmt::Display {
     0
 }
 
-#[cfg(test)]
+//#[cfg(test)]
 mod tests {
     use super::*;
-    const INPUT: &str = "##########
+    const _INPUT: &str = "##########
 #..O..O.O#
 #......O.#
 #.OO..O.O#
@@ -31,13 +31,13 @@ vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
 ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
 v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^";
 
-    #[test]
-    fn part_1() {
-        assert_eq!(part1(INPUT).to_string(), String::from("10092"))
+    //#[test]
+    fn _part_1() {
+        assert_eq!(part1(_INPUT).to_string(), String::from("10092"))
     }
 
     // #[test]
-    fn _part2() {
-        assert_eq!(part2(INPUT).to_string(), String::from("0"))
+    fn _part_2() {
+        assert_eq!(part2(_INPUT).to_string(), String::from("0"))
     }
 }
