18291c3af25c20e1100e9fe4f1ed2a70267598aa
Max <max.heinzer397@gmx.ch>
2024-12-18 18:47:47 +0100

    remove duplicate code

--- src/solutions/year2024/day18.rs
+++ src/solutions/year2024/day18.rs
@@ -7,45 +7,10 @@ use crate::utils::parsing::ByteParsing;
 const SIZE: u8 = 70;
 
 pub fn part1(input: &str) -> impl std::fmt::Display {
-    // idea: do bfs and return as soon at goal is reached
-    let mut lines = input.as_bytes().lines();
-    let mut i = 0;
-    let mut blocked = HashSet::new();
-    while i < 1024 {
-        let (x, y) = lines.next().unwrap().split_once(|&c| c == b',').unwrap();
-        blocked.insert((x.as_num::<u8>(), y.as_num::<u8>()));
-        i += 1;
-    }
-
-    let mut queue = VecDeque::from([(0, (0, 0))]);
-    while !queue.is_empty() {
-        let pos = queue.pop_front().unwrap();
-        if pos.1 == (SIZE, SIZE) {
-            return pos.0;
-        }
-
-        // if is blocked or already visited
-        if !blocked.insert(pos.1) {
-            continue;
-        }
-
-        if (pos.1).0 > 0 {
-            queue.push_back((pos.0 + 1, ((pos.1).0 - 1, (pos.1).1)))
-        }
-        if (pos.1).1 > 0 {
-            queue.push_back((pos.0 + 1, ((pos.1).0, (pos.1).1 - 1)))
-        }
-        if (pos.1).0 < SIZE {
-            queue.push_back((pos.0 + 1, ((pos.1).0 + 1, (pos.1).1)))
-        }
-        if (pos.1).1 < SIZE {
-            queue.push_back((pos.0 + 1, ((pos.1).0, (pos.1).1 + 1)))
-        }
-    }
-    -1
+    get_path(input, 1024).unwrap()
 }
 
-fn has_path(input: &str, iterations: usize) -> bool {
+fn get_path(input: &str, iterations: usize) -> Option<i32> {
     let mut lines = input.as_bytes().lines();
     let mut i = 0;
     let mut blocked = HashSet::new();
@@ -59,7 +24,7 @@ fn has_path(input: &str, iterations: usize) -> bool {
     while !queue.is_empty() {
         let pos = queue.pop_front().unwrap();
         if pos.1 == (SIZE, SIZE) {
-            return true;
+            return Some(pos.0);
         }
 
         // if is blocked or already visited
@@ -80,7 +45,7 @@ fn has_path(input: &str, iterations: usize) -> bool {
             queue.push_back((pos.0 + 1, ((pos.1).0, (pos.1).1 + 1)))
         }
     }
-    false
+    None
 }
 
 pub fn part2(input: &str) -> impl std::fmt::Display {
@@ -89,10 +54,10 @@ pub fn part2(input: &str) -> impl std::fmt::Display {
     let mut max = lines.clone().count();
     let mut i = min + (max - min) / 2;
     loop {
-        if has_path(input, i) && !has_path(input, i + 1) {
+        if get_path(input, i).is_some() && get_path(input, i + 1).is_none() {
             break;
         }
-        if has_path(input, i) {
+        if get_path(input, i).is_some() {
             min = i + 1;
         } else {
             max = i;
