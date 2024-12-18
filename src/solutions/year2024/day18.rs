d5d41e7c7f2a81836522705b3f621fff3cccfd40
Max <max.heinzer397@gmx.ch>
2024-12-18 18:30:23 +0100

    feat(d18): initial

--- src/solutions/year2024/day18.rs
+++ src/solutions/year2024/day18.rs
@@ -1,23 +1,103 @@
-pub fn part1(_input: &str) -> impl std::fmt::Display {
-    0
-}
+use std::collections::{HashSet, VecDeque};
 
-pub fn part2(_input: &str) -> impl std::fmt::Display {
-    0
-}
+use bstr::ByteSlice;
 
-// #[cfg(test)]
-mod tests {
-    use super::*;
-    const _INPUT: &str = "";
+use crate::utils::parsing::ByteParsing;
 
-    // #[test]
-    fn _part1() {
-        assert_eq!(part1(_INPUT).to_string(), String::from("0"))
+const SIZE: u8 = 70;
+
+pub fn part1(input: &str) -> impl std::fmt::Display {
+    // idea: do bfs and return as soon at goal is reached
+    let mut lines = input.as_bytes().lines();
+    let mut i = 0;
+    let mut blocked = HashSet::new();
+    while i < 1024 {
+        let (x, y) = lines.next().unwrap().split_once(|&c| c == b',').unwrap();
+        blocked.insert((x.as_num::<u8>(), y.as_num::<u8>()));
+        i += 1;
     }
 
-    // #[test]
-    fn _part2() {
-        assert_eq!(part2(_INPUT).to_string(), String::from("0"))
+    let mut queue = VecDeque::from([(0, (0, 0))]);
+    while !queue.is_empty() {
+        let pos = queue.pop_front().unwrap();
+        if pos.1 == (SIZE, SIZE) {
+            return pos.0;
+        }
+
+        // if is blocked or already visited
+        if !blocked.insert(pos.1) {
+            continue;
+        }
+
+        if (pos.1).0 > 0 {
+            queue.push_back((pos.0 + 1, ((pos.1).0 - 1, (pos.1).1)))
+        }
+        if (pos.1).1 > 0 {
+            queue.push_back((pos.0 + 1, ((pos.1).0, (pos.1).1 - 1)))
+        }
+        if (pos.1).0 < SIZE {
+            queue.push_back((pos.0 + 1, ((pos.1).0 + 1, (pos.1).1)))
+        }
+        if (pos.1).1 < SIZE {
+            queue.push_back((pos.0 + 1, ((pos.1).0, (pos.1).1 + 1)))
+        }
     }
+    -1
+}
+
+fn has_path(input: &str, iterations: usize) -> bool {
+    let mut lines = input.as_bytes().lines();
+    let mut i = 0;
+    let mut blocked = HashSet::new();
+    while i < iterations {
+        let (x, y) = lines.next().unwrap().split_once(|&c| c == b',').unwrap();
+        blocked.insert((x.as_num::<u8>(), y.as_num::<u8>()));
+        i += 1;
+    }
+
+    let mut queue = VecDeque::from([(0, (0, 0))]);
+    while !queue.is_empty() {
+        let pos = queue.pop_front().unwrap();
+        if pos.1 == (SIZE, SIZE) {
+            return true;
+        }
+
+        // if is blocked or already visited
+        if !blocked.insert(pos.1) {
+            continue;
+        }
+
+        if (pos.1).0 > 0 {
+            queue.push_back((pos.0 + 1, ((pos.1).0 - 1, (pos.1).1)))
+        }
+        if (pos.1).1 > 0 {
+            queue.push_back((pos.0 + 1, ((pos.1).0, (pos.1).1 - 1)))
+        }
+        if (pos.1).0 < SIZE {
+            queue.push_back((pos.0 + 1, ((pos.1).0 + 1, (pos.1).1)))
+        }
+        if (pos.1).1 < SIZE {
+            queue.push_back((pos.0 + 1, ((pos.1).0, (pos.1).1 + 1)))
+        }
+    }
+    false
+}
+
+pub fn part2(input: &str) -> impl std::fmt::Display {
+    let mut lines = input.as_bytes().lines();
+    let mut min = 0;
+    let mut max = lines.clone().count();
+    let mut i = min + (max - min) / 2;
+    loop {
+        if has_path(input, i) && !has_path(input, i + 1) {
+            break;
+        }
+        if has_path(input, i) {
+            min = i + 1;
+        } else {
+            max = i;
+        }
+        i = min + (max - min) / 2;
+    }
+    lines.nth(i).unwrap().to_str().unwrap().to_owned()
 }
