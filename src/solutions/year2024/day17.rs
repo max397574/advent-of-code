f00113984cde6cd29aa36f0ffba0a50648849f11
Max <max.heinzer397@gmx.ch>
2024-12-18 13:06:15 +0100

    feat: performance

--- src/solutions/year2024/day17.rs
+++ src/solutions/year2024/day17.rs
@@ -27,52 +27,50 @@ pub fn part1(input: &str) -> &str {
         let mut out_vals = 0;
         let mut input = input.as_ptr();
         input = input.add("Register A: ".len());
-        let mut reg_a = swar_parsing(*(input as *const u64));
+
+        let mut reg_a = swar_parsing(input.cast::<u64>().read_unaligned());
         let mut reg_b = 0;
         let mut reg_c = 0;
         input = input.add("\nRegister B: 0\nRegister C: 0\n\nProgram: ".len() + 8);
-        let mut programm = [0; 16];
+        let mut program = [0; 16];
         let mut i = 0;
         while i < 16 {
-            *programm.get_unchecked_mut(i) = unchecked_sub(*input.offset(0), b'0');
+            *program.get_unchecked_mut(i) = unchecked_sub(*input.offset(0), b'0');
             input = input.add(2);
             i += 1;
         }
-        // failing: Program: 2,4,1,1,7,5,1,5,4,5,0,3,5,5,3,0
-        // working: Program: 2,4,1,1,7,5,1,5,4,3,5,5,0,3,3,0
         loop {
             reg_b = reg_a % 8;
 
-            reg_b ^= programm[3] as u32;
+            reg_b ^= program[3] as u32;
 
             reg_c = reg_a >> reg_b;
 
-            if programm[6] == 1 {
-                reg_b ^= programm[7] as u32;
-            } else if programm[6] == 4 {
+            if program[6] == 1 {
+                reg_b ^= program[7] as u32;
+            } else if program[6] == 4 {
                 reg_b ^= reg_c;
-            } else if programm[6] == 0 {
                 reg_a >>= 3;
             }
 
-            if programm[8] == 4 {
+            if program[8] == 4 {
                 reg_b ^= reg_c;
-            } else if programm[8] == 0 {
+            } else if program[8] == 0 {
                 reg_a >>= 3;
-            } else if programm[8] == 1 {
-                reg_b ^= programm[9] as u32;
+            } else if program[8] == 1 {
+                reg_b ^= program[9] as u32;
             }
 
-            if programm[10] == 5 {
+            if program[10] == 5 {
                 *OUTPUT.get_unchecked_mut(out_vals * 2) = (reg_b & 0b111) as u8 + b'0';
                 out_vals += 1;
                 reg_a >>= 3;
             } else {
-                if programm[10] == 4 {
+                if program[10] == 4 {
                     reg_b ^= reg_c;
-                } else if programm[10] == 1 {
-                    reg_b ^= programm[11] as u32;
-                } else if programm[10] == 0 {
+                } else if program[10] == 1 {
+                    reg_b ^= program[11] as u32;
+                } else if program[10] == 0 {
                     reg_a >>= 3;
                 }
 
@@ -109,37 +107,50 @@ pub fn part2(input: &str) -> u64 {
             let mut reg_a = a;
             let mut reg_b = 0;
             let mut reg_c = 0;
-            let mut ip = 0;
-            while ip < program.len() {
-                let instruction = program[ip];
-                let operand = program[ip + 1];
-                let combo_operand = || match operand {
-                    0..=3 => operand,
-                    4 => reg_a,
-                    5 => reg_b,
-                    6 => reg_c,
-                    _ => unreachable!(),
-                };
-                match instruction {
-                    0 => reg_a >>= combo_operand(),
-                    1 => reg_b ^= program[ip + 1],
-                    2 => reg_b = combo_operand() % 8,
-                    3 => {
-                        if reg_a != 0 {
-                            ip = program[ip + 1] as usize;
-                            continue;
-                        }
-                    }
-                    4 => reg_b ^= reg_c,
-                    5 => {
-                        out.push(combo_operand() & 0b111);
-                    }
-                    6 => reg_b = reg_a >> combo_operand(),
-                    7 => reg_c = reg_a >> combo_operand(),
-                    _ => unreachable!(),
+            loop {
+                reg_b = reg_a % 8;
+
+                reg_b ^= program[3];
+
+                reg_c = reg_a >> reg_b;
+
+                if program[6] == 1 {
+                    reg_b ^= program[7];
+                } else if program[6] == 4 {
+                    reg_b ^= reg_c;
+                } else if program[6] == 0 {
+                    reg_a >>= 3;
                 }
-                ip += 2;
+
+                if program[8] == 4 {
+                    reg_b ^= reg_c;
+                } else if program[8] == 0 {
+                    reg_a >>= 3;
+                } else if program[8] == 1 {
+                    reg_b ^= program[9];
+                }
+
+                if program[10] == 5 {
+                    out.push(reg_b & 0b111);
+                    reg_a >>= 3;
+                } else {
+                    if program[10] == 4 {
+                        reg_b ^= reg_c;
+                    } else if program[10] == 1 {
+                        reg_b ^= program[11];
+                    } else if program[10] == 0 {
+                        reg_a >>= 3;
+                    }
+
+                    out.push(reg_b & 0b111);
+                }
+
+                // always have 3,0
+                if reg_a == 0 {
+                    break;
+                }
             }
+
             if program.ends_with(&out) {
                 if out.len() == 16 {
                     return a;
