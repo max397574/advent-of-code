2ab1b2b6e4f7c431c0b917a741fb72c527effd22
Max <max.heinzer397@gmx.ch>
2024-12-26 12:50:47 +0100

    feat: 25p1 inital solution

--- src/solutions/year2024/day25.rs
+++ src/solutions/year2024/day25.rs
@@ -1,23 +1,86 @@
-pub fn part1(_input: &str) -> usize {
-    0
+pub fn part1(input: &str) -> usize {
+    let mut keys = Vec::new();
+    let mut locks = Vec::new();
+    for block in input.split("\n\n") {
+        let mut key_or_lock = vec![0, 0, 0, 0, 0];
+        for line in block.lines() {
+            for (idx, char) in line.chars().enumerate() {
+                if char == '#' {
+                    key_or_lock[idx] += 1;
+                }
+            }
+        }
+        if block.starts_with('#') {
+            locks.push(key_or_lock);
+        } else {
+            keys.push(key_or_lock);
+        }
+    }
+    let mut count = 0;
+    for key in keys {
+        for lock in locks.iter() {
+            if key.iter().zip(lock).all(|(k, l)| k + l <= 7) {
+                count += 1;
+            }
+        }
+    }
+    count
 }
 
 pub fn part2(_input: &str) -> usize {
     0
 }
 
-// #[cfg(test)]
+#[cfg(test)]
 mod tests {
     use super::*;
-    const _INPUT: &str = "";
+    const INPUT: &str = "#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
 
-    // #[test]
-    fn _part1() {
-        assert_eq!(part1(_INPUT).to_string(), String::from("0"))
+#####
+##.##
+.#.##
+...##
+...#.
+...#.
+.....
+
+.....
+#....
+#....
+#...#
+#.#.#
+#.###
+#####
+
+.....
+.....
+#.#..
+###..
+###.#
+###.#
+#####
+
+.....
+.....
+.....
+#....
+#.#..
+#.#.#
+#####";
+
+    #[test]
+    fn part_1() {
+        assert_eq!(part1(INPUT).to_string(), String::from("3"))
     }
 
     // #[test]
     fn _part2() {
-        assert_eq!(part2(_INPUT).to_string(), String::from("0"))
+        assert_eq!(part2(INPUT).to_string(), String::from("0"))
     }
 }
