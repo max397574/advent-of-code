dd1e44afa9cea60c4e4c52bfd31874a2cff99a91
Max <max.heinzer397@gmx.ch>
2025-12-01 14:14:23 +0100

    d1 kinda optimizes solutions

--- src/solutions/year2025/day1.rs
+++ src/solutions/year2025/day1.rs
@@ -1,45 +1,87 @@
-use bstr::ByteSlice;
+use std::intrinsics::{unchecked_mul, unchecked_sub};
 
-use crate::utils::parsing::ByteParsing;
+use bstr::ByteSlice;
 
 /// L: 01001100 -> -1
 /// R: 01010010 -> 1
 #[inline(always)]
 fn lr_hash(byte: u8) -> i32 {
-    1 - 2 * (((byte & 4) >> 2) as i32)
+    unsafe { std::intrinsics::unchecked_sub(1, (((byte & 4) >> 2) as i32) << 1) }
 }
 
 pub fn part1(input: &str) -> impl std::fmt::Display + use<> {
-    let mut current = 50;
-    let mut zero_count = 0;
-    input.as_bytes().lines().for_each(|line| {
-        let delta = lr_hash(line[0]) * line[1..].as_num::<i32>();
-        current += delta;
-        current = current.rem_euclid(100);
-        if current == 0 {
-            zero_count += 1;
+    unsafe {
+        let mut current: i32 = 50;
+        let mut zero_count: u32 = 0;
+        let input = input.as_bytes();
+        let len = input.len();
+        let mut input = input.as_ptr();
+        let end_ptr = input.add(len);
+        while input < end_ptr {
+            let delta_sign = lr_hash(*input.offset(0));
+            input = input.add(1);
+            let mut delta: i32 = (unchecked_sub(*input.offset(0), b'0')) as i32;
+            if *input.offset(2) == b'\n' {
+                delta = delta.unchecked_mul(10);
+                delta = delta.unchecked_add((unchecked_sub(*input.offset(1), b'0')) as i32);
+                input = input.add(1);
+            } else if *input.offset(3) == b'\n' {
+                delta = unchecked_mul((unchecked_sub(*input.offset(1), b'0')) as i32, 10);
+                delta = delta.unchecked_add((unchecked_sub(*input.offset(2), b'0')) as i32);
+                input = input.add(2);
+            }
+            input = input.add(2);
+
+            delta = delta.unchecked_mul(delta_sign);
+            current = current.unchecked_add(delta);
+            current = current.rem_euclid(100);
+            if current == 0 {
+                zero_count += 1;
+            }
         }
-    });
-    zero_count
+        zero_count
+    }
 }
 
 pub fn part2(input: &str) -> impl std::fmt::Display + use<> {
-    let mut current = 50;
-    let mut zero_count = 0;
-    input.as_bytes().lines().for_each(|line| {
-        let delta = lr_hash(line[0]) * line[1..].as_num::<i32>();
-        let new_tmp = current + (delta % 100);
-        current += delta;
-        current = current.rem_euclid(100);
-        zero_count += delta / 100;
-        if current == 0 {
-            zero_count += 1;
+    unsafe {
+        let mut current = 50;
+        let mut zero_count = 0;
+        let input = input.as_bytes();
+        let len = input.len();
+        let mut input = input.as_ptr();
+        let end_ptr = input.add(len);
+        while input < end_ptr {
+            let delta_sign = lr_hash(*input.offset(0));
+            input = input.add(1);
+            let mut delta: i32 = (unchecked_sub(*input.offset(0), b'0')) as i32;
+            if *input.offset(2) == b'\n' {
+                delta = delta.unchecked_mul(10);
+                delta = delta.unchecked_add((unchecked_sub(*input.offset(1), b'0')) as i32);
+                input = input.add(1);
+            } else if *input.offset(3) == b'\n' {
+                delta = delta.unchecked_mul(100);
+                delta = delta.unchecked_add(unchecked_mul(
+                    (unchecked_sub(*input.offset(1), b'0')) as i32,
+                    10,
+                ));
+                delta = delta.unchecked_add((unchecked_sub(*input.offset(2), b'0')) as i32);
+                input = input.add(2);
+            }
+            input = input.add(2);
+
+            delta = delta.unchecked_mul(delta_sign);
+
+            if delta >= 0 {
+                zero_count += (current + delta) / 100;
+            } else {
+                zero_count += ((100 - current) % 100 - delta) / 100;
+            }
+            current += delta;
+            current = current.rem_euclid(100);
         }
-        if !(0..=100).contains(&new_tmp) {
-            zero_count += 1;
-        }
-    });
-    zero_count
+        zero_count
+    }
 }
 
 #[cfg(test)]
