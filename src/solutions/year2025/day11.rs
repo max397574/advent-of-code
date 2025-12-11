0ee984592629453722a9a24a483fe8084f8d8994
Max <max.heinzer397@gmx.ch>
2025-12-11 10:26:31 +0100

    d11

--- src/solutions/year2025/day11.rs
+++ src/solutions/year2025/day11.rs
@@ -1,23 +1,120 @@
-pub fn part1(_input: &str) -> impl std::fmt::Display + use<> {
-    0
-}
+use std::collections::{HashMap, VecDeque};
 
-pub fn part2(_input: &str) -> impl std::fmt::Display + use<> {
-    0
-}
+fn toposort(adj: &HashMap<&str, Vec<&str>>) -> Vec<String> {
+    let vertices: Vec<&str> = adj.keys().copied().collect();
 
-// #[cfg(test)]
-mod tests {
-    use super::*;
-    const _INPUT: &str = "";
-
-    // #[test]
-    fn _part_1() {
-        assert_eq!(part1(_INPUT).to_string(), String::from("0"))
+    let mut indeg: HashMap<&str, usize> = HashMap::new();
+    for &v in &vertices {
+        indeg.insert(v, 0);
+    }
+    for neighbors in adj.values() {
+        for v in neighbors {
+            *indeg.entry(v).or_insert(0) += 1;
+        }
     }
 
-    // #[test]
-    fn _part_2() {
-        assert_eq!(part2(_INPUT).to_string(), String::from("0"))
+    let mut queue = VecDeque::new();
+    for &v in &vertices {
+        if indeg[&v] == 0 {
+            queue.push_back(v);
+        }
+    }
+
+    let mut order = Vec::new();
+    while let Some(u) = queue.pop_front() {
+        order.push(u.to_owned());
+        if let Some(neighbors) = adj.get(&u) {
+            for &v in neighbors {
+                let deg = indeg.get_mut(&v).unwrap();
+                *deg -= 1;
+                if *deg == 0 {
+                    queue.push_back(v);
+                }
+            }
+        }
+    }
+    order
+}
+
+pub fn part1(input: &str) -> impl std::fmt::Display + use<> {
+    path_count(input, "you", "out")
+}
+
+pub fn part2(input: &str) -> impl std::fmt::Display + use<> {
+    path_count(input, "svr", "fft")
+        * path_count(input, "fft", "dac")
+        * path_count(input, "dac", "out")
+        + path_count(input, "svr", "dac")
+            * path_count(input, "dac", "fft")
+            * path_count(input, "fft", "out")
+}
+
+fn path_count(input: &str, from: &str, to: &str) -> usize {
+    let mut adj = HashMap::new();
+    for line in input.lines() {
+        let (vertex, neighbours) = line.split_once(": ").unwrap();
+        let neighbours: Vec<_> = neighbours.split(' ').collect();
+        adj.insert(vertex, neighbours);
+    }
+
+    let order = toposort(&adj);
+    let rev_order: Vec<_> = order.iter().rev().cloned().collect();
+
+    let mut dp: HashMap<&str, usize> = HashMap::new();
+
+    for u in &rev_order {
+        if u == to {
+            dp.insert(to, 1);
+            continue;
+        }
+
+        if let Some(neighbors) = adj.get(&u[..]) {
+            let mut paths = 0;
+            for &v in neighbors {
+                paths += dp.get(v).copied().unwrap_or(0);
+            }
+            dp.insert(u, paths);
+        }
+    }
+
+    dp.get(from).copied().unwrap_or(0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const INPUT: &str = "aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out\n";
+
+    const INPUT2: &str = "svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out\n";
+
+    #[test]
+    fn part_1() {
+        assert_eq!(part1(INPUT).to_string(), String::from("5"))
+    }
+
+    #[test]
+    fn part_2() {
+        assert_eq!(part2(INPUT2).to_string(), String::from("2"))
     }
 }
